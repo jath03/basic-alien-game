7c02ce867b14972cef42f75693f4b23c1a6774de
jath03 <jackthughes@yahoo.com>
2022-02-01 16:07:55 -0700

    Added intro screen

--- game.py
+++ game.py
@@ -12,10 +12,7 @@ class GameManager:
         self.done = False
 
     def game_over(self, won: bool):
-        if won:
-            print("Hooray!")
-        else:
-            print("You suck")
+        print("Hooray!" if won else "You suck")
 
     def update(self) -> bool:
         if not self.ship.update(self.alien_mgr.aliens) or not self.alien_mgr.update(self.ship.bullets):
@@ -33,14 +30,31 @@ if __name__ == '__main__':
     window = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Alien Invasion")
 
-    game_mgr = GameManager(window)
+    font = pygame.font.Font(None, 50)
+    window.blit(font.render("Space Invaders", True, (57, 255, 20)), (WIDTH/2 - 120, HEIGHT/2 - 50))
+    font2 = pygame.font.Font(None, 25)
+    window.blit(font2.render("Press <space> to begin", True, (57, 255, 20)), (WIDTH/2 - 90, HEIGHT/2))
+    pygame.display.flip()
     running = True
+    started = False
 
-    while running:
+    while not started:
         for event in pygame.event.get():
             # Checking if window was closed or if q or escape keys were pressed
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and (event.key == pygame.K_q or event.key == pygame.K_ESCAPE)):
+                started = True
                 running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                started = True
+        pygame.time.wait(50)
+    pygame.time.wait(250)
+
+    game_mgr = GameManager(window)
+
+    while running:
+        for event in pygame.event.get(pygame.QUIT):
+            # Checking if window was closed or if q or escape keys were pressed
+            running = False
 
         window.fill(pygame.color.Color(0, 0, 0))
         running = game_mgr.update()
